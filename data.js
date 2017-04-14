74970269702ab19a1894f3f875abbff99bfef420
Taylor Scafe <scafetaylor@gmail.com>
2017-04-13 21:48:34 -0500

    Separated Address. Added Dummy Website

--- data.js
+++ data.js
@@ -1,16 +1,24 @@
 company = [{		
 	title: "Pierce's Dairy Cup",
 	image: "images/peirces.jpg",
-	description: "Small Burger, BBQ and Ice Cream joint.",
-	address: "440 W Main St, Lamoni, IA 50140",
+	description: "Small Burger, BBQ and Ice Cream joint",
+	addressStreet: "440 W Main Street",
+	addressPOBox: "",
+	addressCity: "Lamoni",
+	addressState: "IA",
+	addressZip: "50140",
 	phone:"(641) 784-3494",
-	website: "No Website"
+	website: "http://alt.graceland.edu/~tds2"
 },
 {	
 	title: "Pizza Shack",
 	image: "images/pizzashack.jpg",
-	description: "Local Pizza shop that serves more than pizza",
-	address: "303 E Main St, Lamoni, IA 50140",
+	description: "Local pizza shop that serves more than pizza",
+	addressStreet: "303 E Main Street",
+	addressPOBox: "",
+	addressCity: "Lamoni",
+	addressState: "IA",
+	addressZip: "50140",
 	phone:"(641) 784-3396",
-	website: "No Website"
+	website: ""
 }]
